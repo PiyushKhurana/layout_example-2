3dd2ee54ed5c0d69f4608fd2a2fa24fdf2e4ee32
PiyushKhurana <khuranapiyush27@gmail.com>
2019-07-26 19:41:29 +0530

    build navigator fn v1  - general icon (not specific)

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,7 +4,25 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Paytm"),
+        ),
+        bottomNavigationBar: buildNavigator(),
+      ),
+    );
+  }
+
+  Widget buildNavigator() {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home,color: Colors.blue,), title: Text('Home',style: TextStyle(color: Colors.blue),)),
+        BottomNavigationBarItem(icon: Icon(Icons.local_mall,color: Colors.blue), title: Text('Mall',style: TextStyle(color: Colors.blue),)),
+        BottomNavigationBarItem(icon: Icon(Icons.add_a_photo,color: Colors.blue), title: Text('Scan',style: TextStyle(color: Colors.blue),)),
+        BottomNavigationBarItem(icon: Icon(Icons.attach_money,color: Colors.blue), title: Text('Bank',style: TextStyle(color: Colors.blue),)),
+        BottomNavigationBarItem(icon: Icon(Icons.message,color: Colors.blue,), title: Text('Inbox',style: TextStyle(color: Colors.blue),)),
+      ],
+
     );
   }
 }
